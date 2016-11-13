3bf20b82d522ce8692efa4bcc6b156ffd801fce9
tonytran <tony.tran@wne.edu>
2016-11-12 23:15:25 -0500

    added summary functon

--- content_based/content_based.py
+++ content_based/content_based.py
@@ -3,85 +3,115 @@ import requests
 import queue
 import re
 import nltk
+import json
 from collections import Counter
 from bs4 import BeautifulSoup
 from google import search
 from stop_words import get_stop_words
 from stemming.porter2 import stem
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from heapq import *
 #lemma = nltk.corpus.wordnet.WordNetLemmatizer()
-stop_words = get_stop_words('english')
-non_word_list = ['[',']','*', '(',')','\\','/']
+
+non_word_list = ['[',']','*', '(',')','\\','/','&']
 word_count = []
-stop_words2 = []
-f = open('stop_words.txt', 'r')
+lemma = WordNetLemmatizer()
 stop = stopwords.words('english')
-for word in f:
-    stop_words2.append(word)
-    stop_words2.append(word.upper())
-f.close()
+
 def get_phrase():
     phrase =  str(input("Input phrase to search articles for: "))
 
     return phrase
-
-def main():
-    encountered_list = []
-
-    phrase = get_phrase()
+def get_key_words(phrase, gen_sums=None):
     url_list = []
     #f = open('something.html','w')
+    if gen_sums == None:
 
-    for x in search(phrase, stop = 1):
-        #url_list += [x]
-        urls = requests.get(x)
+        for x in search(phrase, stop = 1):
+            #url_list += [x]
+            urls = requests.get(x)
 
-    #f.write(urls.text)
-    #f.close()
+        #f.write(urls.text)
+        #f.close()
 
-        soup = BeautifulSoup(urls.text,"html.parser")
-        for script in soup(["script", "style"]):
-            script.extract()    # rip it out
+            soup = BeautifulSoup(urls.text,"html.parser")
+            for script in soup(["script", "style"]):
+                script.extract()    # rip it out
 
-        # get text
-        text = soup.get_text()
+            # get text
+            text = soup.get_text()
 
-        # break into lines and remove leading and trailing space on each
-        lines = (line.strip() for line in text.splitlines())
-        # break multi-headlines into a line each
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        # drop blank lines
-        text = '\n'.join(chunk for chunk in chunks if chunk)
+            # break into lines and remove leading and trailing space on each
+            lines = (line.strip() for line in text.splitlines())
+            # break multi-headlines into a line each
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            # drop blank lines
+            text = '\n'.join(chunk for chunk in chunks if chunk)
 
-        for word in text.split():
-            word = word.lower()
-            if word not in stop:
-                #if word not in stop_words2:
-                    #if word not in stop:
+            for word in text.split():
+                word = word.lower()
+                if word not in stop:
+                    #if word not in stop_words2:
+                        #if word not in stop:
 
-                if word not in non_word_list:
-                    if not word.isdigit():
-                        stemword = stem(word)
-                        word_count.append(stemword)
+                    if word not in non_word_list:
+                        if not word.isdigit():
+                            stemword = lemma.lemmatize(word)
+                            #stemword = stem(stemword)
+                            word_count.append(stemword)
 
-                        #else:
-                            #word_count[stemword] += 1
-
-
-        #for key in word_count:
-            #print(key, ': ',word_count[key])
-    #filtered_word_list = word_count[:] #make a copy of the word_list
-    #filtered_words = [word for word in word_count if word not in stopwords.words('english')]
+                            #else:
+                                #word_count[stemword] += 1
 
 
-    cnt = Counter(word_count)
-    queue2 = []
-    #print(cnt)
-    print(type(cnt.most_common()))
+            #for key in word_count:
+                #print(key, ': ',word_count[key])
+        #filtered_word_list = word_count[:] #make a copy of the word_list
+        #filtered_words = [word for word in word_count if word not in stopwords.words('english')]
 
-    not_done = len(queue2)
 
-    for i in cnt.most_common()[:6]:
+        cnt = Counter(word_count)
+        queue2 = []
+        #print(cnt)
 
-main()
+        not_done = len(queue2)
+
+        return cnt.most_common()[:6]
+
+    else: #done filtering
+        for x in search(phrase, stop = 1):
+            url_list += [x]
+        return url_list
+
+def main():
+
+    phrase = get_phrase()
+    top_5 = get_key_words(phrase)
+    resulting_words = []
+    for i in top_5:
+        resulting_words += [i[0] + ' ']
+
+    #(resulting_words)
+    new_phrase = ''.join(resulting_words)
+    print("new phrase to search withL ",new_phrase)
+    urls = get_key_words(new_phrase, gen_sums = 1)
+
+    for url in urls:
+        get_summary(url)
+def get_summary(url):
+    api_key = open('api_key.txt','r').readline().strip()
+
+    params = {"url": url, "apikey":api_key}
+    r = requests.get("https://api.havenondemand.com/1/api/sync/extractconcepts/v1", params = params)
+    sum_list = []
+    try:
+        list2 = r.json()['concepts']
+        for dic in list2:
+            sum_list += [dic['concept'] + ' ']
+        summary = ''.join(sum_list)
+        print(summary)
+    except KeyError:
+        print("Sorry a summary could not be generated for this link")
+if __name__ == '__main__':
+    main()
